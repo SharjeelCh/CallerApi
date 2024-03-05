bcf43ab1ea87fa5fcf24c348f2f1cbf08fe2f21d
SharjeelCh <sharjeelh6451@gmail.com>
2024-03-05 20:04:37 +0500

    Optimised db connection code

--- backend/Connection/dbConnection.js
+++ backend/Connection/dbConnection.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
+require('dotenv').config('../.env')
+const string=process.env.CONNECTION_STRING   // most optimal way to connect to the database
+
 const connectDb = async () => {
   try {
     const connect = await mongoose.connect(
-       'mongodb+srv://sharjeel:1234@cluster0.byu2acx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+      'mongodb+srv://sharjeel:1234@cluster0.byu2acx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     );
     console.log(
-      `MongoDb Connected: `,connect.connection.host,connect.connection.name,
+      `MongoDb Connected\n`,'Host : ',connect.connection.host,' Db Name: ',connect.connection.name,
     );
   } catch (error) {
     console.log(`Error Connecting: ${error.message}`);
