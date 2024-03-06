005e7e7f097d125d5e9843d0b97052b8c4796810
SharjeelCh <sharjeelh6451@gmail.com>
2024-03-06 11:01:10 +0500

    header updated

--- src/Components/Tabbar.js
+++ src/Components/Tabbar.js
@@ -1,27 +1,64 @@
-import React from 'react'
-import '../CSS/index.css'
+import React, { useState } from "react";
+import "../CSS/index.css";
+import { FaHome, FaPersonBooth, FaPhone } from "react-icons/fa";
 
 export default function Tabbar() {
+  const [active, setactive] = useState(false);
+  const [active1, setactive1] = useState(false);
+  const [active2, setactive2] = useState(false);
 
   return (
-    <div className='tab_container flex justify-content-space-between bg-blue-500 text-white p-4'>
+    <div className="tab_container flex justify-between  bg-blue-500 text-white p-4">
       <div>
         <h1>CallerApp</h1>
       </div>
-      <div className='flex'>
-        <div className='tab_item'>
-            <i className='fas fa home'/>
-            <span>Home</span>
+      <div className="items_container flex gap-x-7">
+        <div
+          className="tab_item bg-blue-500 hover:text-blue-800"
+          onMouseOver={() => {
+            setactive(true);
+          }}
+          onMouseLeave={() => {
+            setactive(false);
+          }}
+        >
+          {active ? (
+            <FaHome size={23} />
+          ) : (
+            <span className="hover:text-green">Home</span>
+          )}
         </div>
-        <div className='tab_item'>
-            <i className='fas fa home'/>
-            <span>Contacts</span>
+        <div
+          className="tab_item bg-blue-500 hover:text-blue-800"
+          onMouseOver={() => {
+            setactive1(true);
+          }}
+          onMouseLeave={() => {
+            setactive1(false);
+          }}
+        >
+          {active1 ? (
+            <FaPersonBooth size={23} />
+          ) : (
+            <span className="hover:text-green">Contacts</span>
+          )}
         </div>
-        <div className='tab_item'>
-            <i className='fas fa home'/>
-            <span>Contact Us</span>
+        <div
+          className="tab_item bg-blue-500 hover:text-blue-800"
+          onMouseOver={() => {
+            setactive2(true);
+          }}
+          onMouseLeave={() => {
+            setactive2(false);
+          }}
+        >
+          {active2 ? (
+            <FaPhone size={23} />
+          ) : (
+            <span className="hover:text-green">Contact Us</span>
+          )}
         </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
